f07ad82b8e51469664ae117df1252a5c7b14219e
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-04-14 14:21:25 +0530

    added logic for App.js createContext

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
-import ChildA from './components/childA'
+import React, {createContext} from "react";
+import ChildA from './components/ChildA'
+
+const data_name = createContext(); 
+const data_gender = createContext(); 
 
 function App(){
-  <ChildA/>
+  const name = "Tauqeer Ahmad"
+  const gender = "Male"
+  return (
+    <>
+    <data_name.Provider value={name}>
+    <data_gender.Provider value={gender}> 
+    <ChildA />
+    </data_gender.Provider>
+    </data_name.Provider> 
+    </>
+  ); 
+  
 }
 
 export default App; 
+export {data_gender, data_name}
